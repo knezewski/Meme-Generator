2ff8ecc35c9776d590d487119236cf4985629929
Andrei Kniazev <knezewski@yandex.ru>
2020-09-21 23:49:47 +0300

    upload files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
+
+export default function App() {
+
+   return (
+      <Fragment>
+         <Header />
+         <MemeGenerator />
+      </Fragment>
+   )
+};
+
